86223b8a4b488353c65626350f9c4aa8adc1259f
israbaracho <israbaracho@gmail.com>
2025-11-09 22:01:55 -0300

    change to typescript

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer>
-      <p>© 2025 First Aid App</p>
-    </footer>
-  )
-}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2025 First Aid App</p>
+    </footer>
+  )
+}
